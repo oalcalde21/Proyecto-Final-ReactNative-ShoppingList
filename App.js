a30f71f0d8ad0d36b6fd4be83d006d536f53b718
oalcalde21 <108088299+oalcalde21@users.noreply.github.com>
2023-03-20 21:09:31 -0300

    se modifica app.js

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
-import { ButtonDeletedItems, Footer, ItemList, ItemStatusColor, KeyboardAvoidingView, Modal, NewItemHeader } from "./src/components";
-import { Poppins_Bold, Poppins_Regular, useFonts } from 'expo-font';
 import React, { useCallback, useEffect, useState } from "react";
 
+import { ActivityIndicator } from 'react-native';
 import AppNavigator from './src/navigation';
-import DeletedItemsPage from './src/screens/DeletedItemsPage';
-import Logo from "./src/components/Logo/Logo";
-import StartPage from './src/screens/StartPage';
+import { useFonts } from 'expo-font';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -16,21 +13,12 @@ export default function App() {
   
     // Fonts
     const [fontsLoaded] = useFonts({
-      'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
-      'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf'),
+      "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+      "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
     });
-  
-    const onLayoutRootView = useCallback(async () => {
-      if (fontsLoaded) {
-        await SplashScreen.hideAsync();
-      }
-    }, [fontsLoaded]);
-    
-     
-    // Esta parte es para que no se vea el splash screen hasta que se carguen las fuentes
-    // Va siempre al final y antes del return
+   
     if (!fontsLoaded) {
-      return null;
+      return <ActivityIndicator/>;
     }  
   
   return (
