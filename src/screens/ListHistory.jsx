0fb6ecdad67acc43c9f5886ffc9578f63c31a0ce
oalcalde21 <108088299+oalcalde21@users.noreply.github.com>
2023-04-02 18:59:19 -0300

    se agrega lista de items en History a la espera del funcionamiento del POST y GET

--- src/screens/ListHistory.jsx
+++ src/screens/ListHistory.jsx
@@ -1,12 +1,40 @@
+import { ItemList, Modal } from "../components";
+import React, { useCallback, useEffect, useState } from "react";
 import { StyleSheet, Text, View } from 'react-native'
+import { useDispatch, useSelector } from "react-redux";
 
-import React from 'react'
+import Logo from "../components/Logo/Logo";
 
 const ListHistory = () => {
+  
+  const itemsState = useSelector(state => state.startPage.items);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+  
+  const openModal = (item) => {
+    setSelectedItem(item);
+    setModalVisible(true);
+  };
+  
+  const onCancelModal = () => {
+    setModalVisible(!modalVisible);
+  };
+
+  const onDeleteModal = (id, item) => {
+    setModalVisible(!modalVisible);
+    addDeletedItemToState(id, item);
+  };
+
+  // Todo esto se sustituye por el dispatch de HistoryLoader con las listas guardadas en Firebase
+
   return (
-    <View style={[styles.home]}>
-      <Text>ListHistory</Text>
-    </View>
+    <View style={[styles.home]}> 
+        <Logo/>
+        {/* LIST COMPONENT */}
+        <ItemList items={itemsState} openModal={openModal} />      
+        {/* MODAl COMPONENT */}
+        <Modal modalVisible={modalVisible} selectedItem={selectedItem} onCancelModal={onCancelModal} onDeleteModal={onDeleteModal}/>
+      </View>
   )
 }
 
