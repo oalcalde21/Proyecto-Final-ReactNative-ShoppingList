f0c162bbe395002c86b7919424c9a7ef9260a9a7
oalcalde21 <108088299+oalcalde21@users.noreply.github.com>
2023-04-12 21:38:52 -0300

    se agrega configuracion de sqllite

--- App.js
+++ App.js
@@ -4,8 +4,19 @@ import { ActivityIndicator } from 'react-native';
 import AppNavigator from './src/navigation';
 import { Provider } from 'react-redux';
 import React from "react";
+import { init } from './src/db';
 import store from './src/store';
 import { useFonts } from 'expo-font';
+
+init().then(
+  () => {
+    console.log('Initialized database');
+  }
+).catch(err => {
+    console.log('Initializing db failed.');
+    console.log(err);
+  }
+)
 
 export default function App() {
   
