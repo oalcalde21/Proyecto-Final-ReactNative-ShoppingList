dddf86bf6e418a5a382f51259afd3ad8f8d20464
oalcalde21 <108088299+oalcalde21@users.noreply.github.com>
2023-04-12 22:03:17 -0300

    se agrega redux - persist

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import * as Font from 'expo-font';
 
 import { ActivityIndicator } from 'react-native';
 import AppNavigator from './src/navigation';
+import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux';
 import React from "react";
 import { init } from './src/db';
 import store from './src/store';
 import { useFonts } from 'expo-font';
+
+import {store, storePersisted} from './src/s'
 
 init().then(
   () => {
@@ -32,7 +35,12 @@ export default function App() {
   
   return (
     <>
-      <Provider store={store}><AppNavigator/></Provider>
+      {/* <Provider store={store}><AppNavigator/></Provider> */}
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={storePersisted}>
+          <AppNavigator />
+        </PersistGate>
+      </Provider>
     </>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,17 @@
 import {applyMiddleware, combineReducers, createStore} from 'redux';
+import { persistReducer, persistStore } from 'redux-persist'
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import deletedItemReducer from './reducers/DeleteItemsPage.reducer';
 import historyLoaderReducer from './reducers/HistoryLoader.reducer';
 import itemReducer from './reducers/StartPage.reducer';
 import logInReducer from './reducers/LogIn.reducer';
 import thunk from 'redux-thunk';
+
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage
+  }
 
 const RootReducer = combineReducers({ 
     startPage:itemReducer,
@@ -14,3 +21,4 @@ const RootReducer = combineReducers({
 });
 
 export default createStore(RootReducer, applyMiddleware(thunk));
+export const storePersisted = persistStore(store);
